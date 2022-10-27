24ae93d603fbffc8e4f775b68fdcaa4b4734296c
jkalcarl <anthonygref2021@gmail.com>
2022-10-26 23:09:55 -0500

    Se modifico el archivo del codigo

---
+++ ejer.cpp
@@ -0,0 +1,24 @@
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int numero;
+cout<<"Ingrese  un numero entero: ";
+cin>>numero;
+if(numero%2==0){
+cout<<"El numero: "<<numero<<" es numero par";
+}else{
+cout<<"El numero: "<<numero<<" es numero impar";
+}
+return 0;
+}
+
+
+
+
+
+
+
